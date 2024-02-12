5cf5065802132165641810118e4297a668079833
Lawrence Wong <Lawrence Wong@10.20.19.196>
2024-02-12 08:51:48 -0500

    d

--- src/assignments/Demo1.java
+++ src/assignments/Demo1.java
@@ -5,6 +5,18 @@ public class Demo1 {
 	public static void main(String[] args) {
 		
 		
+		System.out.println("This was created on laptop.");
+
+		System.out.println("This was created on Github.");
+
+		System.out.println("This was created on laptop.");
+		System.out.println("This was created on Github.");
+		System.out.println("This was created on Github.");
+
+		System.out.println("This was created on laptop.");
+
+		System.out.println("This was created on Github.");
+
 	}
 
 }
